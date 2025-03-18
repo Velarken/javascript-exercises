26d4448c15f19edbd4ee6b67458b55d9e48a9900
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2025-03-18 11:22:58 -0400

    Passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    // repeat a given string a given amount of times
+    // expected 'heyheyhey'
+    if (times < 0)return 'ERROR';
+    let string = "";
+    for (let i = 0; i < times; i++) {
+        string += word;
+    }
+    return string;
 };
 
 // Do not edit below this line
