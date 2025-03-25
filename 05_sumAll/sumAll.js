b3c27e511d9d6bba411ccb8ea7a34bded9b4a9e3
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-03-25 10:43:42 -0400

    Passed first 2 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sumNumbers = 0;
+    while (num1 <= num2) {
+        sumNumbers += num1;
+        ++num1;
+    }
+    return sumNumbers;
 };
 
 // Do not edit below this line
