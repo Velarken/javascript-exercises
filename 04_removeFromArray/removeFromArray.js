fbaae7fc3fbc5250b85b8bca87cc39e66b756069
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-03-25 10:19:25 -0400

    Passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(givenArray, ...numbers) {
+    // Determine what is in the array
+    return givenArray.filter(number => !numbers.includes(number));
+    // Create an array to hold the remaining values
+    // Return new array
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
