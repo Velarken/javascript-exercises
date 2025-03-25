4714e3c390d2619ee17adc0fd4e8b78f0d9855e4
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-03-25 11:10:15 -0400

    Passed 5 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,22 @@
 const sumAll = function(num1, num2) {
     let sumNumbers = 0;
-    while (num1 <= num2) {
-        sumNumbers += num1;
-        ++num1;
+    let counter = num1;
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (isNaN(num1) || isNaN(num2)) {
+        return "ERROR";
+    } else {
+        if(num1 < num2) {
+            while (num1 <= num2) {
+                sumNumbers += num1;
+                ++num1;
+            }
+        } else {
+            while (num1 >= num2){
+            sumNumbers += num1;
+            --num1;
+            }
+        }
     }
     return sumNumbers;
 };
