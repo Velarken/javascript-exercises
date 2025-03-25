5c310a99da7bc10ed87e275f5eb6a31085978778
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-03-25 11:30:58 -0400

    Passed all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (Number.isInteger((year/4))) {
+        if (Number.isInteger((year/100)) && !Number.isInteger((year/400))) {
+            return false;
+        } else {
+            return true;
+        }
 
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
