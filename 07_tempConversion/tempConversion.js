3a3a55bc9cf6e562e226257454374abddc58b767
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-03-28 11:52:51 -0400

    Passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  // Formula to use:  (x-32)*(5/9)
+  let convertedTemp = ((temperature - 32)* (5/9))
+  let roundedCelciusTemp = convertedTemp.toFixed(1);
+  // Round answer to 1 decimal place
+  return parseFloat(roundedCelciusTemp);
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  // Formula to use:  (x + (9/5) + 32)
+  const convertedTemp = ((temperature * (9/5)) + 32);
+  let roundedFahrenheitTemp = convertedTemp.toFixed(1);
+
+  return parseFloat(roundedFahrenheitTemp);
+  
 };
 
 // Do not edit below this line
